2884a61edbae8294d0ac9972d4b44692ef542adb
Kemal Kanok <kemalkanok@gmail.com>
2014-03-17 20:34:34 +0200

    lamp + sublime install initial commit

---
+++ linux/start.sh
@@ -0,0 +1,8 @@
+sudo apt-get install apache2
+sudo apt-get install php5 libapache2-mod-php5
+sudo apt-get install mysql-server
+sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin
+sudo service apache2 restart
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo apt-get update
+sudo apt-get install sublime-text
