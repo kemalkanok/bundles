87805f3a4f4fc2e9aef4b75caab2fc57815f7aab
Kemal Kanok <kemalkanok@gmail.com>
2014-04-17 12:28:50 +0300

    auto installer updated

--- linux/ubuntu_lamp_install.sh
+++ linux/ubuntu_lamp_install.sh
@@ -1,8 +1,8 @@
-sudo apt-get install apache2
-sudo apt-get install php5 libapache2-mod-php5
-sudo apt-get install mysql-server
-sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin
+sudo apt-get install apache2 -y
+sudo apt-get install php5 libapache2-mod-php5 -y
+sudo apt-get install mysql-server -y
+sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
 sudo service apache2 restart
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
-sudo apt-get update
-sudo apt-get install sublime-text
+sudo apt-get update 
+sudo apt-get install sublime-text -y
